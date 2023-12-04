703a3d8b5ada5fdffc901baa38a8a764ae9489db
kirillprogs <kirillopervussini@gmail.com>
2023-12-04 02:19:40 +0000

    Database added

--- database.sql
+++ database.sql
@@ -1,2 +1,72 @@
-/* DROP TABLE ...;*/
-/* CREATE TABLE ...;*/
+DROP TABLE person;
+DROP TABLE deposit;
+DROP TABLE transferDaemon;
+DROP TABLE transfer;
+DROP TABLE card;
+DROP TABLE account;
+
+CREATE TABLE account
+(
+    ipn       INTEGER PRIMARY KEY,
+    pib       VARCHAR(50) NOT NULL,
+    age       INT         NOT NULL,
+    init_date DATE DEFAULT CURRENT_DATE,
+    email     VARCHAR(50)
+);
+
+CREATE TABLE card
+(
+    card_number CHAR(16) PRIMARY KEY,
+    pin_code char(4) NOT NULL,
+    ipn INTEGER NOT NULL,
+    balance NUMERIC(8,2) NOT NULL,
+    init_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    end_date DATE NOT NULL DEFAULT (CURRENT_DATE + interval '5 years'),
+    FOREIGN KEY (ipn)
+        REFERENCES account (ipn)
+
+);
+
+CREATE TABLE transfer
+(
+    tr_id SERIAL PRIMARY KEY,
+    card_from CHAR(16) NOT NULL,
+    card_to CHAR(16) NOT NULL,
+    amount NUMERIC(8,2) NOT NULL,
+    tr_d_id INTEGER,
+    FOREIGN KEY (card_from)
+        references card (card_number),
+    FOREIGN KEY (card_to)
+        references card (card_number),
+    FOREIGN KEY (tr_d_id)
+        REFERENCES transferDaemon( tr_d_id)
+);
+
+CREATE TABLE transferDaemon
+(
+    tr_d_id SERIAL PRIMARY KEY,
+    card_from CHAR(16) NOT NULL,
+    card_to CHAR(16) NOT NULL,
+    amount NUMERIC(8,2) NOT NULL,
+    next_transfer_date DATE NOT NULL,
+    frequency INTERVAL NOT NULL,
+    FOREIGN KEY (card_from)
+        references card (card_number),
+    FOREIGN KEY (card_to)
+        references card (card_number)
+);
+
+CREATE TABLE deposit
+(
+    dep_id SERIAL PRIMARY KEY,
+    ipn INTEGER NOT NULL,
+    payout_card char(16) NOT NULL,
+    amount NUMERIC(8,2) NOT NULL,
+    intr_rate_year NUMERIC(4,3) NOT NULL,
+    end_date DATA NOT NULL,
+    years INTEGER NOT NULL,
+    FOREIGN KEY (ipn)
+        REFERENCES account (ipn),
+    FOREIGN KEY (payout_card)
+        references card (card_number)
+);
