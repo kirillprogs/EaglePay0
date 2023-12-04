9e4130cce5869d6449bde815f09d8bc3df042da9
kirillprogs <kirillopervussini@gmail.com>
2023-12-04 02:48:43 +0000

    Database: account:birth_date

--- database.sql
+++ database.sql
@@ -7,33 +7,33 @@ DROP TABLE account;
 
 CREATE TABLE account
 (
-    ipn       INTEGER PRIMARY KEY,
-    pib       VARCHAR(50) NOT NULL,
-    age       INT         NOT NULL,
-    init_date DATE DEFAULT CURRENT_DATE,
-    email     VARCHAR(50)
+    ipn        INTEGER     PRIMARY KEY,
+    pib        VARCHAR(50) NOT NULL,
+    birth_date DATE        NOT NULL,
+    init_date  DATE        DEFAULT CURRENT_DATE,
+    email      VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE card
 (
-    card_number CHAR(16) PRIMARY KEY,
-    pin_code char(4) NOT NULL,
-    ipn INTEGER NOT NULL,
-    balance NUMERIC(8,2) NOT NULL,
-    init_date DATE NOT NULL DEFAULT CURRENT_DATE,
-    end_date DATE NOT NULL DEFAULT (CURRENT_DATE + interval '5 years'),
+    card_number CHAR(16)     PRIMARY KEY,
+    pin_code    CHAR(4)      NOT NULL,
+    ipn         INTEGER      NOT NULL,
+    balance     NUMERIC(8,2) NOT NULL,
+    init_date   DATE         NOT NULL DEFAULT CURRENT_DATE,
+    end_date    DATE         NOT NULL DEFAULT (CURRENT_DATE + interval '5 years'),
     FOREIGN KEY (ipn)
         REFERENCES account (ipn)
 );
 
 CREATE TABLE transferDaemon
 (
-    tr_d_id SERIAL PRIMARY KEY,
-    card_from CHAR(16) NOT NULL,
-    card_to CHAR(16) NOT NULL,
-    amount NUMERIC(8,2) NOT NULL,
-    next_transfer_date DATE NOT NULL,
-    frequency INTERVAL NOT NULL,
+    tr_d_id            SERIAL       PRIMARY KEY,
+    card_from          CHAR(16)     NOT NULL,
+    card_to            CHAR(16)     NOT NULL,
+    amount             NUMERIC(8,2) NOT NULL,
+    next_transfer_date DATE         NOT NULL,
+    frequency          INTERVAL     NOT NULL,
     FOREIGN KEY (card_from)
         references card (card_number),
     FOREIGN KEY (card_to)
@@ -42,11 +42,11 @@ CREATE TABLE transferDaemon
 
 CREATE TABLE transfer
 (
-    tr_id SERIAL PRIMARY KEY,
-    card_from CHAR(16) NOT NULL,
-    card_to CHAR(16) NOT NULL,
-    amount NUMERIC(8,2) NOT NULL,
-    tr_d_id INTEGER,
+    tr_id     SERIAL       PRIMARY KEY,
+    card_from CHAR(16)     NOT NULL,
+    card_to   CHAR(16)     NOT NULL,
+    amount    NUMERIC(8,2) NOT NULL,
+    tr_d_id   INTEGER,
     FOREIGN KEY (card_from)
         references card (card_number),
     FOREIGN KEY (card_to)
@@ -57,13 +57,13 @@ CREATE TABLE transfer
 
 CREATE TABLE deposit
 (
-    dep_id SERIAL PRIMARY KEY,
-    ipn INTEGER NOT NULL,
-    payout_card char(16) NOT NULL,
-    amount NUMERIC(8,2) NOT NULL,
+    dep_id SERIAL  PRIMARY KEY,
+    ipn            INTEGER      NOT NULL,
+    payout_card    CHAR(16)     NOT NULL,
+    amount         NUMERIC(8,2) NOT NULL,
     intr_rate_year NUMERIC(4,3) NOT NULL,
-    end_date DATE NOT NULL,
-    years INTEGER NOT NULL,
+    end_date       DATE         NOT NULL,
+    years          INTEGER      NOT NULL,
     FOREIGN KEY (ipn)
         REFERENCES account (ipn),
     FOREIGN KEY (payout_card)
